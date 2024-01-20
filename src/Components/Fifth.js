f61aeeac3e7e9880f49b8029ca238c201fe55ba7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-20 22:26:47 +0100

    not finish  the fifth   Question :art:

--- src/Components/Fifth.js
+++ src/Components/Fifth.js
@@ -6,7 +6,41 @@ Consigne : appeler l'api : https://jsonplaceholder.typicode.com/todos/ et affich
 */
 
 export const Fifth = () => {
+  const [todos, setTodos] = useState([])
+  const [todosPerPage, setTodosPerPage] = useState(10)
+  const [currentPages, setCurrentPages] = useState(1)
+
+  const numOfTotalPages = Math.ceil(todos.length /todosPerPage)
+  const pages  =[...Array(numOfTotalPages +1).keys()].slice(1)
+
+
+  useEffect(() =>{
+    try {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos/")
+        .then(response => setTodos(response.data));
+
+    } catch (error) {
+      console.log(error);
+    }
+  },[])
+
+
+
   return (
-    <div>Pagination</div>
+    <div>
+      {
+        todos.map((todo) => (
+        <p>{todo.title}</p>
+        ))}
+        <span>Prev</span>
+        <p>
+          {
+            pages.map((page) => (
+              <span key={page}>{`${page} | `}</span>
+          ))}
+        </p>
+        <span>Next </span>
+    </div>
   )
 }
