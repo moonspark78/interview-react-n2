8c287548af0fd29fd135eeb3798690bb60b0a3ba
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-20 15:59:44 +0100

    finish the third  Question

--- src/Components/third.js
+++ src/Components/third.js
@@ -11,5 +11,19 @@ const person ={
 */
 
 /* pour cela nous allons utiliser Object.entries()  c une méthode */
-
+/* le resultat est : [[],[]]   */
 console.log(Object.entries(person));
+
+
+/* Nous allons essayer une deuxieme méthodes */
+const getObjectEntries = (obj) => {
+    const objKey = Object.keys(obj);
+    console.log(objKey);
+
+    objKey.map(key =>{
+        const value = obj[key];
+        return [key, value]
+    })
+}
+
+getObjectEntries(person);
