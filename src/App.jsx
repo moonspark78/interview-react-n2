3a861c0a00511162c28e0f68faec423fd6947b37
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-23 10:54:36 +0100

    create the CallBack component

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ import { useState } from 'react';
 
 function App() {
   const [uiColor, setUiColor] = useState(null)
+
+  const changeColor = (color) => {
+    setUiColor(color)
+  };
+
+
   return (
     <div className="App">
       {/* <Fourthh/> */}
        {/* <Fifth/>  */}
        {/* <Sixth/> */}
-       <div className='to'></div>
-       <Callback/>
+       <div className='to' style={{backgroundColor: uiColor}}></div>
+       <Callback colo={changeColor}/>
     </div>
   );
 }
--- src/Components/Callback.jsx
+++ src/Components/Callback.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import "./Call.css"
 
-export const Callback = () => {
+export const Callback = ({colo}) => {
     const [search, setSearch] = useState("");
 
 
-    const changeInput = () => {};
-    
+    const changeInput = (e) => {
+        const color = e.target.value;
+        setSearch(color);
+        colo(color);
+
+    };
+
   return (
     <div>
         <input 
@@ -14,6 +19,8 @@ export const Callback = () => {
             placeholder="Enter your name" 
             name="text"  
             class="input-field" 
+            onChange={changeInput}
+            value={search}
         />
     </div>
   )
