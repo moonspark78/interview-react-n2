da7ba1e7173a91b8db30e3d63ca20c0f4a527d2b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-23 10:05:23 +0100

    create the CallBack component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css';
 import {Fourthh} from "./Components/Fourthh"
 import {Fifth} from "./Components/Fifth"
 import {Sixth} from "./Components/Sixth"
+import {Callback} from "./Components/Callback"
+import { useState } from 'react';
 
 function App() {
+  const [uiColor, setUiColor] = useState(null)
   return (
     <div className="App">
       {/* <Fourthh/> */}
        {/* <Fifth/>  */}
-       <Sixth/>
+       {/* <Sixth/> */}
+       <div></div>
+       <Callback/>
     </div>
   );
 }
